89dec6330dcb33ac48f09d111954c7dda312d981
Bruce Quach <quachbruce@gmail.com>
2022-01-23 02:05:53 -0600

    Drawing with P5 example 4

--- Drawing_with_P5_Example_4_2022_01_23_03_08_31/sketch.js
+++ Drawing_with_P5_Example_4_2022_01_23_03_08_31/sketch.js
@@ -3,5 +3,28 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(0,0,255);
+
+  //creating green background circle
+  fill(0, 135, 0);
+  strokeWeight(5);
+  stroke(255,255,255);
+  arc(200, 200, 200, 200, 0, 2 * PI);
+  
+  fill(255, 0, 0);
+  strokeWeight(5);
+  stroke(255,255,255);
+  beginShape();
+  vertex(200, 95);
+  vertex(175, 170);
+  vertex(100, 170);
+  vertex(165, 220);
+  vertex(150, 290);
+  vertex(200, 255);
+  vertex(250, 290);
+  vertex(235, 220);
+  vertex(300, 170);
+  vertex(225, 170);
+  vertex(200, 95);
+  endShape();
 }
