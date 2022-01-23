04c2e8c5d375ef113806477b7e1b681e97881cfa
Bruce Quach <quachbruce@gmail.com>
2022-01-22 21:05:47 -0600

    Update sketch.js

--- Drawing_with_P5_Example_3_2022_01_23_02_05_16/sketch.js
+++ Drawing_with_P5_Example_3_2022_01_23_02_05_16/sketch.js
@@ -1,7 +1,27 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
 }
 
 function draw() {
-  background(220);
+  background(0,0,0);
+  
+  noStroke();
+
+  //Pac-Man image
+  fill(255, 255, 0);
+  arc(100, 100, 150, 150, 0, 3 * PI  / 4);
+  arc(100, 100, 150, 150, 5 * PI  / 4, 2 * PI);  
+
+  //Red ghost image
+  fill(255,0,0);
+  arc(300, 100, 150, 150, PI, 2 * PI);
+  rect(225, 100, 150, 70);
+
+  fill(255, 255, 255);
+  arc(265, 100, 45, 45, 0, 2 * PI);
+  arc(335, 100, 45, 45, 0, 2 * PI);
+
+  fill(0, 0, 255);
+  arc(265, 100, 25, 25, 0, 2 * PI);
+  arc(335, 100, 25, 25, 0, 2 * PI);
 }
