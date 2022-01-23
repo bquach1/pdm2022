d36544a865b5f0de8b22e10d490756ac68e0010c
Bruce Quach <quachbruce@gmail.com>
2022-01-23 02:32:04 -0600

    Syntax changes

--- Drawing with P5 Example 2/sketch.js
+++ Drawing with P5 Example 2/sketch.js
@@ -3,23 +3,22 @@ function setup() {
 }
 
 function draw() {
-  background('white'); 
+  background(255, 255, 255); 
   
   noStroke();
   //red (top) circle
-  let color1 = color(255, 0, 0, 90)
+  let color1 = color(255, 0, 0, 90);
   fill(color1);
   circle(200, 150, 150, 100);
-  let value = alpha(color1);
-  fill(value);
-
   
   //blue (left) circle
-  fill(0,0,255, 90);
+  let color2 = color(0, 0, 255, 90);
+  fill(color2);
   circle(150, 225, 150, 50);
 
   //green (right) circle
-  fill(0,255,0, 90);
+  let color3 = color(0, 255, 0, 90)
+  fill(color3);
   circle(250, 225, 150, 25);
   
 }
