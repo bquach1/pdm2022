66f153750f6e989eeacd9bf838b6a0510c4a2fb0
Bruce Quach <quachbruce@gmail.com>
2022-01-25 21:52:52 -0600

    Paint app file edits and github link file

--- Paint App/sketch.js
+++ Paint App/sketch.js
@@ -16,26 +16,8 @@ function draw() {
   fill("yellow");
   rect(0,60,30);
 
-  fill(0, 255, 0);
+  fill("green");
   rect(0,90,30);
-
-  fill("cyan");
-  rect(0,120,30);
-
-  fill(0,0,255);
-  rect(0,150,30);
-
-  fill("magenta");
-  rect(0,180,30);
-
-  fill(112, 66, 20);
-  rect(0,210,30);
-  
-  fill(255, 255, 255);
-  rect(0,240,30);
-
-  fill(0, 0, 0);
-  rect(0,270,30);
 
   stroke(0);
   strokeWeight(5);
