19738aed6df4280d981edb1b586d1bcae973d915
Bruce Quach <quachbruce@gmail.com>
2022-01-26 23:47:13 -0600

    Edit paint

--- paint-app/sketch.js
+++ paint-app/sketch.js
@@ -6,36 +6,38 @@ function setup() {
 function draw() {
   background(220);
 
+  let rectSize = 30;
+
   noStroke();
   fill(255, 0, 0);
-  rect(0,0,30);
+  rect(0,0,rectSize);
 
   fill("orange");
-  rect(0,30,30);
+  rect(0,30,rectSize);
 
   fill("yellow");
-  rect(0,60,30);
+  rect(0,60,rectSize);
 
   fill(0, 255, 0);
-  rect(0,90,30);
+  rect(0,90,rectSize);
 
   fill("cyan");
-  rect(0,120,30);
+  rect(0,120,rectSize);
 
   fill(0,0,255);
-  rect(0,150,30);
+  rect(0,150,rectSize);
 
   fill("magenta");
-  rect(0,180,30);
+  rect(0,180,rectSize);
 
   fill(112, 66, 20);
-  rect(0,210,30);
+  rect(0,210,rectSize);
   
   fill(255, 255, 255);
-  rect(0,240,30);
+  rect(0,240,rectSize);
 
   fill(0, 0, 0);
-  rect(0,270,30);
+  rect(0,270,rectSize);
 
   stroke(0);
   strokeWeight(5);
